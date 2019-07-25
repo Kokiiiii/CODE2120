839d60be40d7c37c3596b19e1e653e2117eeaf9f
Kokiiiii <Kokiiiii@users.noreply.github.com>
2019-07-25 11:54:16 +1000

    Update views.py

--- mysite/wombat/views.py
+++ mysite/wombat/views.py
@@ -7,4 +7,18 @@ import os, sys
 # Create your views here.
 @csrf_exempt
 def assignment1(request):
-	return JsonResponse({"test":"working"})
+	jsob = {"startNumber": 0, "length": 10, "multiNumber": 2, "maximum": 50000} #defaults
+	log = []
+	if request.method == "POST":
+		try:
+			
+			return JsonResponse({"assignment1":"numarray"})
+
+		except Exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			other = sys.exc_info()[0].__name__
+			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			errorType = str(exc_type)
+			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})		
+	else:
+		return JsonResponse(jsob)		
